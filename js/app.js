10c5916d630377be61fd81ec454ba9c8880d1f8b
marielb <marielbartolome@gmail.com>
2013-01-25 20:39:39 -0500

    added app.js

---
+++ js/app.js
@@ -0,0 +1,10 @@
+/* ALL OUR JS GOES HERE */
+
+$(function(){ //DOM Ready
+
+    $(".gridster ul").gridster({
+        widget_margins: [10, 10],
+        widget_base_dimensions: [100, 100]
+    });
+
+});
